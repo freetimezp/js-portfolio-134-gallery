aea8f86e991c946d902bc704e7c79461c9d9fdbf
freetimezp <kraftukrnet@ukr.net>
2024-10-26 20:47:27 +0300

    03. js-portfolio-134-gallery. PhotoSwipe.

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,27 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }, 200);
 });
+
+
+document.querySelectorAll(".gallery__item").forEach(function (e) {
+    let img = new Image();
+    let hrefURL = e.getAttribute("href");
+
+    img.onload = function () {
+        e.dataset.pswpWidth = this.width * 0.8;
+        e.dataset.pswpHeight = this.height * 0.8;
+    };
+
+    img.src = hrefURL;
+});
+
+
+import PhotoSwiperLightbox from "../libs/PhotoSwipe/photoswipe-lightbox.esm.min.js";
+
+const lightbox = new PhotoSwiperLightbox({
+    gallery: ".gallery",
+    children: ".gallery__item",
+    pswpModule: () => import("../libs/PhotoSwipe/photoswipe.esm.min.js")
+});
+
+lightbox.init();
